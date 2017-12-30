02ee103424203cf956c069cf7a602aafb65a6697
joejb <joebh@hotmail.com>
2017-12-30 10:36:54 -0500

    Create nc_installation.sh

---
+++ nc_installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Negative Color "
+
+sudo apt-get install -y compiz compizconfig-settings-manager compiz-plugins-extra libdecoration0-dev dconf-editor
+
+echo "Menu search for CompizConfig Settings Manager"
+
+echo "Negative"
+
+echo "Enable Negative "
